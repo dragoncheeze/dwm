150408c49ca2af5b66e53420cf03e6fa72910565
dragoncheeze <colby.braden@gmail.com>
2021-04-05 19:27:26 -0500

    changed theme colors for dwm

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#000000";
-static const char col_cyan[]        = "#690000";
+static const char col_cyan[]        = "#294a71";
 static const char col_red[]         = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "fish", NULL };
 static const char *pcmanfm[]  = { "pcmanfm", NULL };
 static const char *browser[]  = { "brave-browser", NULL };
 static const char *rofi[] = { "drun", NULL };
